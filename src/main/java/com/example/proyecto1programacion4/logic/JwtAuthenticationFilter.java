d7d9b8c44ad925ed454339579cec009858a43f7b
KarenAvlo <karen.aviles.lopez@est.una.ac.cr>
2026-05-13 14:17:27 -0600

    Backend creo que listo

---
+++ src/main/java/com/example/proyecto1programacion4/logic/JwtAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.example.proyecto1programacion4.logic;
+
+public class JwtAuthenticationFilter {
+}
---
+++ src/main/java/com/example/proyecto1programacion4/presentation/api/CaracteristicaRestController.java
@@ -0,0 +1,4 @@
+package com.example.proyecto1programacion4.presentation.api;
+
+public class CaracteristicaRestController {
+}
